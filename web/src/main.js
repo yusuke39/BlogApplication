79aef202bdf6384d6f6ba434b29879e745177ee0
yusuke39 <hiranoyusuke39@gmail.com>
2020-07-21 16:33:41 +0900

    vue修正＆vuetifyに変更

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import store from './store/index.js'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
@@ -12,5 +13,6 @@ Vue.use(ElementUI, { locale })
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- web/src/views/adminLogin.vue
+++ web/src/views/adminLogin.vue
@@ -1,48 +1,99 @@
 <template>
-  <div class="login-box">
-    <el-input placeholder="メールアドレス" v-model="email" id="email" class="input-email" clearable></el-input>
-    <el-input placeholder="パスワード" v-model="password" id="password" class="input-password" show-password></el-input>
-    <el-button type="success" class="login-button">ログイン</el-button>
-    <el-row>
-      <el-button type="warning" class="new-register-button">新規会員登録</el-button>
-    </el-row>
-  </div>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+  <v-text-field
+    v-model="name"
+    :counter="10"
+    :rules="nameRules"
+    label="Name"
+    required
+  ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validate
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
 export default {
-  name: 'sample',
-  data () {
-    return {
-      email: '',
-      password: ''
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    select: null,
+    items: [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
+    ],
+    checkbox: false
+  }),
+
+  methods: {
+    validate () {
+      this.$refs.form.validate()
+    },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
     }
   }
 }
 </script>
-
-<style scoped>
-.login-box {
-  width: 500px;
-  margin: 200px auto;
-}
-
-.input-email {
-  width : 450px;
-  margin-bottom: 50px;
-}
-
-.input-password {
-  width : 450px;
-  margin-bottom: 50px;
-}
-
-.login-button{
-  width: 300px;
-  margin-bottom: 30px;
-}
-
-.new-register-button {
-  width: 300px;
-}
-</style>
