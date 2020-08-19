e158523692b6526103d9d9ec3c93ad82be77c09e
yusuke39 <hiranoyusuke39@gmail.com>
2020-08-19 21:42:31 +0900

    管理者情報編集画面

---
+++ web/src/views/blogMainImageUpload.vue
@@ -0,0 +1,117 @@
+<template>
+<div class="icon-file-box">
+  <p class="icon-label">ブログメイン画像</p>
+  <label class="avatar-uploader">
+    <input
+      type="file"
+      @change="onFileChange"
+      accept=".jpeg, .png, .jpg"
+      class="avatar-uploader-inner"
+    >
+    <canvas
+      ref="canvas"
+      class="avatar-uploader-area"
+    ></canvas>
+    <i :class="{ 'fas fa-camera fa-3x' : isShowCameraIcon }"></i>
+  </label>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      blogMainIMage: '',
+      isShowCameraIcon: true
+    }
+  },
+  methods: {
+    onFileChange (e) {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        this.createImage(e.target.result)
+      }
+      reader.readAsDataURL(file)
+    },
+    createImage (file) {
+      const image = new Image()
+      image.crossOrigin = 'Anonymous'
+      image.onload = e => {
+        const resizedBase64 = this.makeResizeImg(image)
+        const resizeBlob = this.base64ToBlob(resizedBase64)
+        const resizeImg = window.URL.createObjectURL(resizeBlob)
+        this.blogMainIMage = resizeImg
+        this.isShowCameraIcon = false
+      }
+      image.src = file
+    },
+    makeResizeImg (image) {
+      const canvas = this.$refs.canvas
+      const ctx = canvas.getContext('2d')
+      const MAX_SIZE = 1000
+      if (image.width < MAX_SIZE && image.height < MAX_SIZE) {
+        [canvas.width, canvas.height] = [image.width, image.height]
+        ctx.drawImage(image, 0, 0)
+        return canvas.toDataURL('image/jpeg')
+      }
+      let dstWidth
+      let dstHeight
+      // 縦横比の計算
+      if (image.width > image.height) {
+        dstWidth = MAX_SIZE
+        dstHeight = (image.height * MAX_SIZE) / image.width
+      } else {
+        dstHeight = MAX_SIZE
+        dstWidth = (image.width * MAX_SIZE) / image.height
+      }
+      canvas.width = dstWidth
+      canvas.height = dstHeight
+      // リサイズ
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dstWidth, dstHeight)
+
+      // data_url形式に変換したものを返す
+      return canvas.toDataURL('image/jpeg')
+    },
+    base64ToBlob (base64) {
+      const bin = atob(base64.replace(/^.*,/, ''))
+      const buffer = new Uint8Array(bin.length)
+      for (let i = 0; i < bin.length; i++) {
+        buffer[i] = bin.charCodeAt(i)
+      }
+      return new Blob([buffer.buffer], {
+        type: 'image/png'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .icon-file-box {
+  text-align: center;
+  height: auto;
+  margin-bottom: 30px;
+ }
+
+ .avatar-uploader > input {
+   display: none;
+ }
+
+ .fa-camera {
+   position: relative;
+   top: -90px;
+ }
+
+ .avatar-uploader-area {
+   display: block;
+   width: 600px;
+   height: auto;
+   margin: 0 auto;
+   background-color: #c4c4c4;
+ }
+
+ .icon-label {
+   color: rgba(0, 0, 0, 0.6);
+ }
+</style>
--- web/src/views/editAdmin.vue
+++ web/src/views/editAdmin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <AdminHeader></AdminHeader>
-    <div class="edit-admin-wrapper">
+    <el-card class="edit-admin-wrapper">
       <form>
         <v-text-field
           class="edit-admin-input"
@@ -14,29 +14,6 @@
           @input="$v.name.$touch()"
           @blur="$v.name.$touch()"
         ></v-text-field>
-        <div class="icon-file-box">
-          <p class="icon-label">アイコン画像</p>
-          <label class="avatar-uploader">
-            <input
-              type="file"
-              @change="onFileChange"
-              class="avatar-uploader-inner"
-            >
-            <img
-              v-show="iconImage"
-              :src="iconImage"
-              class="avatar-uploader">
-            <i :class="{ 'fa-user-alt fas fa-3x' : isShow }"></i>
-          </label>
-        </div>
-        <v-file-input
-          class="edit-admin-input"
-          accept="image/png, image/jpeg, image/bmp"
-          placeholder="アイコン(画像を選択してください)"
-          prepend-icon="mdi-camera"
-          label="アイコン"
-          name="iconImage"
-        ></v-file-input>
         <v-text-field
           class="edit-admin-input"
           v-model="email"
@@ -47,15 +24,18 @@
           @input="$v.email.$touch()"
           @blur="$v.email.$touch()"
         ></v-text-field>
-
+        <UploadUserIcon></UploadUserIcon>
+        <BlogMainImage></BlogMainImage>
         <el-button type="success" @click="submit" class="edit-button">編集完了</el-button>
       </form>
-    </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 import AdminHeader from './adminHeader'
+import UploadUserIcon from './iconImageUpload'
+import BlogMainImage from './blogMainImageUpload'
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, email } from 'vuelidate/lib/validators'
 export default {
@@ -67,10 +47,7 @@ export default {
   data () {
     return {
       name: '',
-      email: '',
-      icon: '',
-      iconImage: '',
-      isShow: true
+      email: ''
     }
   },
   computed: {
@@ -90,32 +67,21 @@ export default {
     }
   },
   methods: {
-    onFileChange (e) {
-      const files = e.target.files || e.dataTransfer.files
-      this.createImage(files[0])
-      this.iconImage = files[0].name
-      this.isShow = false
-    },
-    createImage (file) {
-      const reader = new FileReader()
-      reader.onload = e => {
-        this.iconImage = e.target.result
-      }
-      reader.readAsDataURL(file)
-    },
     submit () {
       this.$v.$touch()
     }
   },
   components: {
-    AdminHeader
+    AdminHeader,
+    UploadUserIcon,
+    BlogMainImage
   }
 }
 </script>
 
 <style>
  .edit-admin-wrapper {
-   width: 500px;
+   width: 800px;
    margin: 100px auto;
  }
 
@@ -145,47 +111,12 @@ export default {
    color: #ffffff;
    width: 100px;
    margin-top: 20px;
+   margin-left: 230px;
  }
 
  .edit-admin-input {
+   width: 400px;
+   margin: 0 auto;
    margin-bottom: 30px;
  }
-
- .icon-file-box {
-   margin: 30px auto;
-  text-align: center;
- }
-
- .avatar-uploader > input {
-   display: none;
- }
-
- .avatar-uploader {
-   display: inline-block;
-   width: 130px;
-   height: 130px;
-   border-radius: 100%;
-   text-align: center;
-   line-height: 140px;
-   background-color: #c4c4c4;
- }
-
- .icon-label {
-   color: rgba(0, 0, 0, 0.6);
- }
-
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 100px;
-  height: 100px;
-  position: relative;
-  top: 35px;
-  text-align: center;
-}
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
 </style>
